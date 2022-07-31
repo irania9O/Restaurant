eefb384d715deecd77766402d2c059c3306386e8
irania90 <wwwwwq37@gmail.com>
2022-07-31 16:48:47 +0430

    add search to food and drink menu.

--- frontend/ui_class/customer.py
+++ frontend/ui_class/customer.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QApplication, QDesktopWidget , QSpinBox 
+from PyQt5.QtWidgets import QDialog, QApplication, QDesktopWidget, QFrame, QSpinBox , QCheckBox , QLabel, QLineEdit , QFormLayout
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.uic import loadUi
 
@@ -15,26 +15,80 @@ class MainScreen(QDialog):
         pixmap = QtGui.QPixmap("frontend/icons/drinks.png").scaled(300, 100)
         self.drinks.setPixmap(pixmap)
 
-        self.cocke_regular.stateChanged.connect(lambda x : self.changer_spin_box(self.cocke_regular.objectName()))
-        self.pepsi_regular.stateChanged.connect(lambda x : self.changer_spin_box(self.pepsi_regular.objectName()))
-        self.sprite_regular.stateChanged.connect(lambda x : self.changer_spin_box(self.sprite_regular.objectName()))
-        self.string_regular.stateChanged.connect(lambda x : self.changer_spin_box(self.string_regular.objectName()))
-        self.coke_1_5.stateChanged.connect(lambda x : self.changer_spin_box(self.coke_1_5.objectName()))
-        self.pepsi_1_5.stateChanged.connect(lambda x : self.changer_spin_box(self.pepsi_1_5.objectName()))
-        self.sprite_1_5.stateChanged.connect(lambda x : self.changer_spin_box(self.sprite_1_5.objectName()))
-        self.string_1_5.stateChanged.connect(lambda x : self.changer_spin_box(self.string_1_5.objectName()))
-        self.cups.stateChanged.connect(lambda x : self.changer_spin_box(self.cups.objectName()))
+        pixmap = QtGui.QPixmap("frontend/icons/foods.png").scaled(300, 100)
+        self.foods.setPixmap(pixmap)
+        
+        pixmap = QtGui.QPixmap("frontend/icons/factor.png").scaled(300, 100)
+        self.factor.setPixmap(pixmap)        
 
-    def changer_spin_box(self , name):
-        counter_drinks = self.findChild(QSpinBox, f"{name}_count")
+        #print(self.lcdNumber.intValue())
+        #self.lcdNumber.display(150)
+        #print(self.lcdNumber.intValue())
+        self.search_food.textChanged.connect(self.doSomething)
+        self.search_drink.textChanged.connect(self.doSomething)
+        
+        self.calendarWidget.setMinimumDate(self.calendarWidget.selectedDate())
+        self.calendarWidget.selectionChanged.connect(self.update_foods)
+        self.update_foods()
+        
+        self.foods_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.foods_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        
+        self.drinks_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.drinks_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        
+    def doSomething(self):
+        print(0)
+        
+    def enable_disable_spin_box(self , name):
+        counter_drinks = self.findChild(QSpinBox, f"{self.sender().objectName()}_count")
         if counter_drinks.isEnabled() :
             counter_drinks.setDisabled(True)
             counter_drinks.setValue(0)
         else:
             counter_drinks.setDisabled(False)
             counter_drinks.setValue(1)
-            
+
+    def change_spin_box(self , value):
+        spin_box = self.sender()         
+        print(spin_box)
+        print(value)
+
+    def update_foods(self):
+        print(self.calendarWidget.selectedDate().toString("yyyy-MM-dd"))
         
+        formFrameFoods = QFrame()
+        self.layout_foods = QFormLayout(formFrameFoods)
+        self.foods_area.setWidget(formFrameFoods)
+        
+        for i in range(5):
+            checkbox = QCheckBox(f"food{i} \t {i*10}$", self, objectName = f"food{i}") 
+            checkbox.setStyleSheet('QCheckBox { font: 10pt "MV Boli"; }')
+            checkbox.stateChanged.connect(lambda x : self.enable_disable_spin_box(x))
+            
+            spinbox = QSpinBox(self, objectName = f"food{i}_count") 
+            spinbox.setStyleSheet('QSpinBox { font: 10pt "MV Boli"; }')
+            spinbox.setDisabled(True)
+            spinbox.valueChanged.connect(lambda x : self.change_spin_box(x))                       
+            
+            self.layout_foods.addRow(checkbox, spinbox)
+            
+        formFrameDrinks = QFrame()
+        self.layout_drinks = QFormLayout(formFrameDrinks)
+        self.drinks_area.setWidget(formFrameDrinks)
+  
+        for i in range(5):
+            checkbox = QCheckBox(f"drink{i} \t {i*10}$", self, objectName = f"drink{i}") 
+            checkbox.setStyleSheet('QCheckBox { font: 10pt "MV Boli"; }')
+            checkbox.stateChanged.connect(lambda x : self.enable_disable_spin_box(x))
+            
+            spinbox = QSpinBox(self, objectName = f"drink{i}_count") 
+            spinbox.setStyleSheet('QSpinBox { font: 10pt "MV Boli"; }')
+            spinbox.setDisabled(True)
+            spinbox.valueChanged.connect(lambda x : self.change_spin_box(x))                       
+            
+            self.layout_drinks.addRow(checkbox, spinbox)
+            
     def centralize(self):
         frameGm = self.frameGeometry()
         screen = QApplication.desktop().screenNumber(
